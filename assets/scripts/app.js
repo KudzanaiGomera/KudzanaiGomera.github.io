bf8303688707a74e5dde7aba0bc5e180a3847091
KudzanaiGomera <kgomera@student.wethinkcode.co.za>
2020-04-07 14:31:55 +0200

    my updated portfolio

---
+++ assets/scripts/app.js
@@ -0,0 +1,82 @@
+const Portfolio = function() {
+    function makeWords() {
+        var words = [{
+            text: "Frameworks",
+            weight: 12.3
+        }, {
+            text: "css3",
+            weight: 8
+        }, {
+            text: "javascript",
+            weight: 14
+        }, {
+            text: "jquery",
+            weight: 3
+        }, {
+            text: "programming",
+            weight: 7
+        }, {
+            text: "python",
+            weight: 10
+        }, {
+            text: "java",
+            weight: 9
+        }, {
+            text: "PHP",
+            weight: 15
+        }, {
+            text: "C/C++",
+            weight: 7
+        }];
+        return words;
+    }
+
+    function makeWordCloud(words) {
+        $('.teaching-domains').jQCloud(words, { delay: 120 });
+    }
+
+    function displayWordCloud() {
+        var count = 1;
+        $(window).on('scroll', function() {
+            var y_scroll_pos = window.pageYOffset;
+            var scroll_pos_test = 2700; // set to whatever you want it to be
+            var words = makeWords();
+            if (y_scroll_pos > scroll_pos_test && count <= 1) {
+                makeWordCloud(words);
+                count++;
+            }
+        });
+    }
+
+    function designForm() {
+        $("#my-modal form").addClass("my-form");
+    }
+
+    function typeAnimation() {
+        Typed.new("#writing-text", {
+            strings: [
+                "am a Full-Stack Web Developer.", "love everything l can do with code.", "and l love teaching people how to", "solve complex problems."
+            ],
+            // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+            stringsElement: null,
+            // typing speed
+            typeSpeed: 1,
+            contentType: 'text',
+            callback: function() {
+                $("#writing-text").css({ "color": "#fff", "background-color": "#C8412B" });
+            },
+            preStringTyped: function() {},
+            onStringTyped: function() {}
+        });
+    }
+
+    return {
+        displayWordCloud: displayWordCloud,
+        typeAnimation: typeAnimation
+    }
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();

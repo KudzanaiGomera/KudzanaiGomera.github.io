fb4a525a4b6a5003f35a416e774c33491a83290b
KudzanaiGomera <kudzie.gom@gmail.com>
2021-01-20 00:18:21 +0200

    updated

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,29 +1,29 @@
 const Portfolio = function() {
     function makeWords() {
         var words = [{
-            text: "Frameworks",
-            weight: 12.3
+            text: "JavaScript",
+            weight: 12
         }, {
-            text: "css3",
+            text: "Css3",
             weight: 8
         }, {
-            text: "javascript",
-            weight: 14
+            text: "Django",
+            weight: 15
         }, {
-            text: "jquery",
-            weight: 3
+            text: "Jquery",
+            weight: 8
         }, {
-            text: "programming",
+            text: "Html5",
             weight: 7
         }, {
-            text: "python",
+            text: "Python",
             weight: 10
         }, {
-            text: "java",
+            text: "Flask",
             weight: 9
         }, {
-            text: "PHP",
-            weight: 15
+            text: "React",
+            weight: 14
         }, {
             text: "C/C++",
             weight: 7
@@ -55,15 +55,15 @@ const Portfolio = function() {
     function typeAnimation() {
         Typed.new("#writing-text", {
             strings: [
-                "am a Full-Stack Web Developer.", "love everything l can do with code.", "and l love teaching people how to", "solve complex problems."
+                "am a Software Engineer."
             ],
             // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
             stringsElement: null,
             // typing speed
-            typeSpeed: 1,
+            typeSpeed: 2,
             contentType: 'text',
             callback: function() {
-                $("#writing-text").css({ "color": "#fff", "background-color": "#C8412B" });
+                $("#writing-text").css({});
             },
             preStringTyped: function() {},
             onStringTyped: function() {}
